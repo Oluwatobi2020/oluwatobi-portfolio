48b9ed15e96f4cf77ad5b60056950c4a5d3cbdf4
oluwatobi <ojedejitobi2020@gmail.com>
2026-05-28 23:46:49 +0100

    new updates

--- src/components/WorkCard.tsx
+++ src/components/WorkCard.tsx
@@ -1,4 +1,12 @@
-import { Grid, Box, Typography, Stack, keyframes, Chip } from "@mui/material";
+import {
+  Grid,
+  Box,
+  Typography,
+  Stack,
+  keyframes,
+  Chip,
+  Link,
+} from "@mui/material";
 import { useState } from "react";
 
 const fadeUp = keyframes`
@@ -6,15 +14,20 @@ const fadeUp = keyframes`
   to   { opacity: 1; transform: translateY(0); }
 `;
 
-const WorkCard = ({ project, index }:any) => {
+const WorkCard = ({ project, index }: any) => {
+  console.log("project", project)
   const [hovered, setHovered] = useState(false);
-  console.log(hovered)
+  console.log(hovered);
+  const handleRedirect = (linkUrl: string) => {
+    window.open(`${linkUrl}`, "_blank");
+  };
   return (
     <Box
       onMouseEnter={() => setHovered(true)}
       onMouseLeave={() => setHovered(false)}
       sx={{
-        py: 4, px: 0,
+        py: 4,
+        px: 0,
         borderTop: "0.5px solid #ECECEC",
         cursor: "pointer",
         transition: "background 0.2s",
@@ -24,32 +37,91 @@ const WorkCard = ({ project, index }:any) => {
         paddingRight: { xs: 2, md: 3 },
         mx: { xs: -2, md: -3 },
       }}
+      onClick={() => handleRedirect(project?.link)}
     >
-      <Grid container spacing={2} sx={{alignItems:"flex-start"}} >
-        <Grid size={{xs:12, md:7}}>
-          <Stack spacing={2} sx={{mb:1, alignItems:"baseline", direction:"row"}}>
-            <Typography variant="body2" sx={{ color: "#BBBBBB", fontFamily: "'DM Sans', sans-serif", fontSize: "0.72rem", minWidth: 24 }}>
+      <Grid container spacing={2} sx={{ alignItems: "flex-start" }}>
+        <Grid size={{ xs: 12, md: 7 }}>
+          <Stack
+            spacing={2}
+            sx={{ mb: 1, alignItems: "baseline", direction: "row" }}
+          >
+            <Typography
+              variant="body2"
+              sx={{
+                color: "#BBBBBB",
+                fontFamily: "'DM Sans', sans-serif",
+                fontSize: "0.72rem",
+                minWidth: 24,
+              }}
+            >
               {project.num}
             </Typography>
-            <Typography variant="h3" sx={{ fontSize: { xs: "1.25rem", md: "1.55rem" }, color: "#111111", lineHeight: 1.2 }}>
+            <Typography
+              variant="h3"
+              sx={{
+                fontSize: { xs: "1.25rem", md: "1.55rem" },
+                color: "#111111",
+                lineHeight: 1.2,
+              }}
+            >
               {project.title}
             </Typography>
           </Stack>
-          <Typography variant="body2" sx={{ color: "#888888", lineHeight: 1.7, ml: "2.5rem", fontSize: "0.88rem" }}>
+          <Typography
+            variant="body2"
+            sx={{
+              color: "#888888",
+              lineHeight: 1.7,
+              ml: "2.5rem",
+              fontSize: "0.88rem",
+            }}
+          >
             {project.desc}
           </Typography>
         </Grid>
-        <Grid size={{xs:12, md:3}} >
-          <Stack sx={{ ml: { xs: "2.5rem", md: 0 }, mt: { xs: 1, md: 0.5 }, flexWrap:"wrap", gap:0.75, direction:"row" }}>
-            {project.tags.map((t:any) => <Chip key={t} label={t} size="small" />)}
+        <Grid size={{ xs: 12, md: 3 }}>
+          <Stack
+            sx={{
+              ml: { xs: "2.5rem", md: 0 },
+              mt: { xs: 1, md: 0.5 },
+              flexWrap: "wrap",
+              gap: 0.75,
+              direction: "row",
+            }}
+          >
+            {project.tags.map((t: any) => (
+              <Chip key={t} label={t} size="small" />
+            ))}
           </Stack>
         </Grid>
-        <Grid size={{xs:12, md:2}} sx={{ display: "flex", alignItems: "flex-start", justifyContent: { xs: "flex-start", md: "flex-end" } }}>
+        <Grid
+          size={{ xs: 12, md: 2 }}
+          sx={{
+            display: "flex",
+            alignItems: "flex-start",
+            justifyContent: { xs: "flex-start", md: "flex-end" },
+          }}
+        >
           <Box sx={{ ml: { xs: "2.5rem", md: 0 }, mt: { xs: 0.5, md: 0.5 } }}>
-            <Typography variant="body2" sx={{ color: "#BBBBBB", fontSize: "0.72rem", fontFamily: "'DM Sans', sans-serif", mb: 0.25 }}>
+            <Typography
+              variant="body2"
+              sx={{
+                color: "#BBBBBB",
+                fontSize: "0.72rem",
+                fontFamily: "'DM Sans', sans-serif",
+                mb: 0.25,
+              }}
+            >
               {project.year}
             </Typography>
-            <Typography variant="body2" sx={{ color: "#888888", fontSize: "0.78rem", fontFamily: "'DM Sans', sans-serif" }}>
+            <Typography
+              variant="body2"
+              sx={{
+                color: "#888888",
+                fontSize: "0.78rem",
+                fontFamily: "'DM Sans', sans-serif",
+              }}
+            >
               {project.type}
             </Typography>
           </Box>
@@ -59,4 +131,4 @@ const WorkCard = ({ project, index }:any) => {
   );
 };
 
-export default WorkCard
+export default WorkCard;
--- src/components/shared/sampleData.ts
+++ src/components/shared/sampleData.ts
@@ -4,42 +4,49 @@ export const projects = [
     desc: "This is an application that allows customers to book their hotel room ahead of visit from any location around the world.",
     tags: ["Next.js, Typescript"],
     year: "2026", type: "Web Development",
+    link:"https://bookinday.com/"
   },
   {
     num: "02", title: "CoralPay Website",
     desc: "This is the website for CoralPay Technology.",
     tags: ["React", "Typescript",],
     year: "2026", type: "Web Application",
+    link:"https://www.coralpay.com/"
   },
   {
     num: "03", title: "Reconciliation Application",
     desc: "This application is use as an automation for reconciliation of settlement reports",
     tags: ["Next.js", "Typescript",],
     year: "2026", type: "Web Application",
+    link:"#"
   },
   {
     num: "04", title: "BiilerOne Web Application",
     desc: "This is a platorm where billers are aggregated and it enables customer to pay for their utility bills all in one place. ",
     tags: ["React", "Javascript"],
-    year: "2025", type: "Mobile App",
+    year: "2025", type: "Web Development",
+    link:"https://bill.coralpay.com/"
   },
   {
     num: "05", title: "Kents and Partners Care",
     desc: "This website is dedicated to enhancing the quality of life for the elderly and mobility-impaired in our community",
     tags: ["React", "Javacript"],
-    year: "2024", type: "Full-Stack",
+    year: "2024", type: "Web Development",
+    link:"https://kentandpartnerscare.com.ng/"
   },
   {
     num: "05", title: "PPAI",
     desc: "This web application was developed to automates and manage customer savings towards desired goals",
     tags: ["Next.js", "Javacript"],
-    year: "2024", type: "Full-Stack",
+    year: "2024", type: "Web Development",
+    link:"https://app.ppai.ng/"
   },
   {
     num: "06", title: "Transaction Made Easy",
     desc: "This is an application that automates almost all teller related operations in a financial institution.",
     tags: ["React", "Javacript"],
-    year: "2024", type: "Full-Stack",
+    year: "2024", type: "Design & Web Development",
+    link:"#"
   },
 ];
 
